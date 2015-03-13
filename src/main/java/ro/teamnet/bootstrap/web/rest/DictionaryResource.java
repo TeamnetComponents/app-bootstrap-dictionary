827c08a81dd2da64beb884ff947d15665670b338
Oana.Mihai <oana.mihai@teamnet.ro>
2015-03-13 22:58:47 +0200

    removed BaseController dependency

--- src/main/java/ro/teamnet/bootstrap/web/rest/DictionaryResource.java
+++ src/main/java/ro/teamnet/bootstrap/web/rest/DictionaryResource.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @RestController
 @RequestMapping("/dictionary")
-public class DictionaryResource extends BaseController {
+public class DictionaryResource {
 
     private final Logger log = LoggerFactory.getLogger(DictionaryResource.class);
 
